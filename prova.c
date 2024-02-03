16dbef07a72418773261a01564258891d6650a64
Asaf Cohen <asafcohen9898@gmail.com>
2024-02-03 16:53:14 +0100

    Aggiunta commento

--- prova.c
+++ prova.c
@@ -6,6 +6,7 @@ void permute(int array[], int start, int end) {
 }
 
 int main() {
+    //int array[] = {1, 2, 3, 4, 5};
     int array[] = {1, 2, 3, 4, 5};
     for (int i = 0; i < 5; i++) printf("%d ", array[i]);
     printf("\n");
