79751631e76f69f09a6aad0a90a880af1564aeb5
Asaf Cohen <asafcohen9898@gmail.com>
2024-02-29 23:18:57 +0100

    Aggiunta opzione ignoraOpt che permette di ignorare il file opt.tour

--- TSP.py
+++ TSP.py
@@ -3,7 +3,8 @@ import random
 import sys
 
 class TSP:
-    def __init__(self, name):
+    def __init__(self, name, ignoraOpt=False):
+        self.ignoraOpt = ignoraOpt
         self.openTSP(name)
 
     def distance(self, a, b):
@@ -39,22 +40,26 @@ class TSP:
                 else:
                     self.adj[i][j] = self.adj[j][i] = self.distance(self.coord[i], self.coord[j])
 
-        # leggo il persorso ottimale
-        f = open(f"ALL-TSP/{name}.opt.tour")
-        self.optTour = [-1 for _ in range(self.numCity)]
-        self.tour = [-1 for _ in range(self.numCity)]
-        data = False
-        i = 0
-        for line in f:
-            if "-1" in line:
-                break
-            if data:
-                # considero i nodi del grafo a partire da 0 e non da 1 come su TSP LIB
-                self.optTour[i] = int(line) - 1
-                i += 1
-            if "TOUR_SECTION" in line:
-                data = True
-        f.close()
+        if not self.ignoraOpt:
+            # leggo il persorso ottimale
+            f = open(f"ALL-TSP/{name}.opt.tour")
+            self.optTour = [-1 for _ in range(self.numCity)]
+            self.tour = [-1 for _ in range(self.numCity)]
+            data = False
+            i = 0
+            for line in f:
+                if "-1" in line:
+                    break
+                if data:
+                    # considero i nodi del grafo a partire da 0 e non da 1 come su TSP LIB
+                    self.optTour[i] = int(line) - 1
+                    i += 1
+                if "TOUR_SECTION" in line:
+                    data = True
+            f.close()
+        else:
+            self.optTour = [-1 for _ in range(self.numCity)]
+            self.tour = [-1 for _ in range(self.numCity)]
         
     def calculateCost(self):
         self.cost = 0
