9b326a7b67cd1cf6d2e4ae94ede80fdeb69507d9
Asaf Cohen <asafcohen9898@gmail.com>
2024-03-07 21:31:04 +0100

    Ottimizzazione nearestInsertion da O(n^3) a O(n^2)

--- TSP.py
+++ TSP.py
@@ -1,6 +1,7 @@
 import math
 import random
 import sys
+import numpy as np
 
 class TSP:
     def __init__(self, name, ignoraOpt=False):
@@ -126,7 +127,7 @@ class TSP:
             self.tour.append(j)
         self.calculateCost()
     
-    #@profile
+    @profile
     def nearestInsertion(self):
         self.tour = [-1, -1]
         self.tour[0] = 0
@@ -160,6 +161,47 @@ class TSP:
                 self.tour.insert(pos, r)
             visited.add(r)
             
+        self.calculateCost()
+    
+    @profile
+    def nearestInsertionNEW(self):
+        n = self.numCity
+        distances = np.array(self.adj)
+        path = [0]  # Inizia da una città arbitraria, in questo caso la prima
+        in_path = {0}
+        min_distance_to_path = np.inf * np.ones(n)
+        nearest_city_to_path = np.zeros(n, dtype=int)
+
+        # Inizializza le distanze minime e le città più vicine per ogni città non nel percorso
+        for i in range(1, n):
+            min_distance_to_path[i] = distances[0, i]
+            nearest_city_to_path[i] = 0
+
+        while len(path) < n:
+            # Trova la città non inserita più vicina a qualsiasi città nel percorso
+            to_insert = np.argmin(min_distance_to_path)
+            min_distance_to_path[to_insert] = np.inf
+
+            # Trova la posizione ottimale per inserire la città trovata
+            best_increase = np.inf
+            best_position = None
+            for i in range(len(path)):
+                next_i = (i + 1) % len(path)
+                increase = distances[path[i], to_insert] + distances[to_insert, path[next_i]] - distances[path[i], path[next_i]]
+                if increase < best_increase:
+                    best_increase = increase
+                    best_position = i + 1
+
+            path.insert(best_position, to_insert)
+            in_path.add(to_insert)
+
+            # Aggiorna le distanze minime e le città più vicine per ogni città non nel percorso
+            for i in range(n):
+                if i not in in_path and distances[to_insert, i] < min_distance_to_path[i]:
+                    min_distance_to_path[i] = distances[to_insert, i]
+                    nearest_city_to_path[i] = to_insert
+
+        self.tour = path
         self.calculateCost()
     
     #@profile
